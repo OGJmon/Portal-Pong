90f2705ed5170a39f16702022670c6dc5612ab2e
OGJmon <jason.halliday@maine.edu>
2024-10-13 12:34:25 -0400

    Remove all the "geek" variables

--- pong.py
+++ pong.py
@@ -14,7 +14,7 @@ PINK = (255, 20, 147)
 # Basic parameters of the screen
 WIDTH, HEIGHT = 900, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Pong")
+pygame.display.set_caption("Portal Pong")
 print(screen.get_size())
 
 clock = pygame.time.Clock() 
@@ -24,17 +24,17 @@ def main():
 	running = True
 
 	# Defining the objects
-	geek1 = Striker(posx=20, posy=0, width=10, height=100, speed=10, color=WHITE, screen=screen)
-	geek2 = Striker(posx=WIDTH-30, posy=0, width=10, height=100, speed=10, color=GREEN, screen=screen)
+	player1 = Striker(posx=20, posy=0, width=10, height=100, speed=10, color=WHITE, screen=screen)
+	player2 = Striker(posx=WIDTH-30, posy=0, width=10, height=100, speed=10, color=GREEN, screen=screen)
 	ball = Ball(posx=WIDTH//2, posy=HEIGHT//2, radius=7, speed=14, color=WHITE, screen=screen)
 	portals = PortalPair(screen=screen, height=50, width=10, color=PINK)
 
-	listOfGeeks = [geek1, geek2]
+	listOfplayers = [player1, player2]
 	list_of_portal_pairs = [portals]
 
 	# Initial parameters of the players
-	geek1Score, geek2Score = 0, 0
-	geek1YFac, geek2YFac = 0, 0
+	player1Score, player2Score = 0, 0
+	player1YFac, player2YFac = 0, 0
  
 	PORTALS_MOVE = pygame.USEREVENT + 1
 	pygame.time.set_timer(PORTALS_MOVE, 5000) 
@@ -48,26 +48,26 @@ def main():
 				running = False
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_UP:
-					geek2YFac = -1
+					player2YFac = -1
 				if event.key == pygame.K_DOWN:
-					geek2YFac = 1
+					player2YFac = 1
 				if event.key == pygame.K_w:
-					geek1YFac = -1
+					player1YFac = -1
 				if event.key == pygame.K_s:
-					geek1YFac = 1
+					player1YFac = 1
 			if event.type == pygame.KEYUP:
 				if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-					geek2YFac = 0
+					player2YFac = 0
 				if event.key == pygame.K_w or event.key == pygame.K_s:
-					geek1YFac = 0
+					player1YFac = 0
 			if event.type == PORTALS_MOVE:
 				for portal_pair in list_of_portal_pairs:
 					portal_pair.update()
 				
 
 		# Collision detection
-		for geek in listOfGeeks:
-			if pygame.Rect.colliderect(ball.getRect(), geek.getRect()):
+		for player in listOfplayers:
+			if pygame.Rect.colliderect(ball.getRect(), player.getRect()):
 				ball.hit()
 		for portal_pair in list_of_portal_pairs:
 			for index, portal in enumerate(portal_pair.portal_list):
@@ -78,18 +78,18 @@ def main():
 						ball.teleport(portals.loc1)
 
 		# Updating the objects
-		geek1.update(geek1YFac)
-		geek2.update(geek2YFac)
+		player1.update(player1YFac)
+		player2.update(player2YFac)
 		point = ball.update()
 		portals.draw()
 
-		# -1 -> Geek_1 has scored
-		# +1 -> Geek_2 has scored
+		# -1 -> player_1 has scored
+		# +1 -> player_2 has scored
 		# 0 -> None of them scored
 		if point == -1:
-			geek1Score += 1
+			player1Score += 1
 		elif point == 1:
-			geek2Score += 1
+			player2Score += 1
 
 		# Someone has scored
 		# a point and the ball is out of bounds.
@@ -98,15 +98,15 @@ def main():
 			ball.reset()
 
 		# Displaying the objects on the screen
-		geek1.display()
-		geek2.display()
+		player1.display()
+		player2.display()
 		ball.display()
 
 		# Displaying the scores of the players
-		geek1.displayScore("Geek_1 : ", 
-						geek1Score, 100, 20, WHITE)
-		geek2.displayScore("Geek_2 : ", 
-						geek2Score, WIDTH-100, 20, WHITE)
+		player1.displayScore("player_1 : ", 
+						player1Score, 100, 20, WHITE)
+		player2.displayScore("player_2 : ", 
+						player2Score, WIDTH-100, 20, WHITE)
 
 		pygame.display.update()
 		clock.tick(FPS)	
